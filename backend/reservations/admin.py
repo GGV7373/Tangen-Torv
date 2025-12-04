23ba140fa9e8e9c8ce2861e667f7d076ed9bb9ff
Victor <GGVictor7373@outlook.com>
2025-12-04 21:00:26 +0100

    Update admin.py

--- backend/reservations/admin.py
+++ backend/reservations/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Table, Reservation
 
-# Register your models here.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+	list_display = ("number", "capacity", "is_active")
+	list_filter = ("is_active",)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+	list_display = ("name", "date", "time", "guests", "table")
+	list_filter = ("date", "time")
